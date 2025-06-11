6932c0981f99d3c36b0e646f1d0388dba077ec8a
HalfTimeOfLife <ec.charbonnier@gmail.com>
2025-06-11 18:29:07 +0200

    add dll and injector code

--- PanoptivApp/PanoptivApp.c
+++ PanoptivApp/PanoptivApp.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <io.h>
 #include <tchar.h>
-#include <wchar.h>
 #include <Windows.h>
 
+bool DllInjector(char* DllPath, char* ExecutablePath) {
+
+}
+
 int main() {
+    printf("[+] Panoptiv application starting ...\n");
 
-	printf("[+] Panoptiv application launched!\n");
+    /* Allocate buffers */
+    char DllPath[512] = { 0 };
+    char ExecutablePath[512] = { 0 };
 
-	HMODULE hDll = LoadLibraryW(_T("PanoptivDLL.dll"));
+    /* Get Dll Path */
+    printf("[+] Enter the dll path : ");
+    scanf_s("%511s", DllPath, (unsigned)_countof(DllPath));
 
-	if (!hDll) {
-		printf("[-] Error while loading library: error %lu\n", GetLastError());
-		return EXIT_FAILURE;
-	}
+    /* Get Executable Path */
+    printf("[+] Enter the executable path : ");
+    scanf_s("%511s", ExecutablePath, (unsigned)_countof(ExecutablePath));
 
-	return EXIT_SUCCESS;
+    if (_access(DllPath, 0) == -1 || _access(ExecutablePath, 0) == -1) {
+        printf("[-] Error: Dll or Executable path does not exist.\n");
+        printf("[!] Dll path : %s\n", DllPath);
+        printf("[!] Executable path : %s\n", ExecutablePath);
+        printf("[!] Please check the paths and try again.\n");
+        return EXIT_FAILURE;
+    }
+    
+    return DllInjector(DllPath, ExecutablePath) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
--- PanoptivDriver/Source.c
+++ PanoptivDriver/Source.c
@@ -1,8 +1,32 @@
 #include <ntddk.h>
 
+#define DLL_PATH L"PanoptivDLL.dll"
+#define TARGET_EXE_NAME L"dummy.exe"
+
+VOID ProcessDetectionRoutine(PEPROCESS Process, HANDLE hProcess, PPS_CREATE_NOTIFY_INFO ProcessInformation)
+{
+    UNREFERENCED_PARAMETER(Process);
+    UNREFERENCED_PARAMETER(hProcess);
+
+
+    if (ProcessInformation && ProcessInformation->ImageFileName) {
+        /* Test wether the newly created process is our target */
+        if (wcsstr(ProcessInformation->ImageFileName->Buffer, TARGET_EXE_NAME)) {
+            DbgPrint("[+] Detected process: %wZ\n", ProcessInformation->ImageFileName);
+            /* INJECT DLL */
+		}
+    }
+}
 
 NTSTATUS DriverUnload(_In_ PDRIVER_OBJECT driverObject) {
     UNREFERENCED_PARAMETER(driverObject);
+
+    NTSTATUS status = PsSetCreateProcessNotifyRoutineEx(ProcessDetectionRoutine, TRUE);
+    if (!NT_SUCCESS(status))
+    {
+        DbgPrint("[!] Failed to remove injection routine: 0x%X\n", status);
+        return status;
+    }
 
     DbgPrint("[-] Panoptiv unloaded\n");
     return STATUS_SUCCESS;
@@ -10,10 +34,17 @@ NTSTATUS DriverUnload(_In_ PDRIVER_OBJECT driverObject) {
 
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT driverObject, _In_ PUNICODE_STRING registryPath) {
     UNREFERENCED_PARAMETER(registryPath);
-
     driverObject->DriverUnload = DriverUnload;
 
-    DbgPrint("[+] Driver loaded\n");
+    
 
+    NTSTATUS status = PsSetCreateProcessNotifyRoutineEx(ProcessDetectionRoutine, FALSE);
+    if (!NT_SUCCESS(status))
+    {
+		DbgPrint("[!] Failed to set injection routine: 0x%X\n", status);
+        return status;
+    }
+
+    DbgPrint("[+] Panoptiv loaded\n");
     return STATUS_SUCCESS;
 }
