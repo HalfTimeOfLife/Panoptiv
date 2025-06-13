7703b781fef12955eb1d7255ea65eb8503376572
HalfTimeOfLife <ec.charbonnier@gmail.com>
2025-06-13 18:56:56 +0200

    Error while allocating memory in the target process : 87

--- PanoptivApp/PanoptivApp.c
+++ PanoptivApp/PanoptivApp.c
@@ -6,6 +6,43 @@
 
 bool DllInjector(char* DllPath, char* ExecutablePath) {
 
+    STARTUPINFOA startupInfo = { 0 };
+    PROCESS_INFORMATION processInfo = { 0 };
+
+    /* Create target process in suspended mode */
+    if (!CreateProcessA(ExecutablePath, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED,
+        NULL, NULL, &startupInfo, &processInfo)) {
+        printf("[-] Error while creating target process : %lu\n", GetLastError());
+        return false;
+    }
+
+    /* Allocate Memory in the process for the DLL */
+    LPVOID pDllPath =
+        VirtualAllocEx(processInfo.hProcess, NULL, strlen(DllPath) + 1,
+            MEM_COMMIT || MEM_RESERVE, PAGE_READWRITE);
+
+    if (!pDllPath) {
+        printf("[-] Error while allocating memory in the target process : "
+            "%lu\n",
+            GetLastError());
+        TerminateProcess(processInfo.hProcess, 1);
+        CloseHandle(processInfo.hThread);
+        CloseHandle(processInfo.hProcess);
+        return false;
+    }
+
+    /* Write DLL path in the process memory */
+    if (!WriteProcessMemory(processInfo.hProcess, pDllPath, DllPath,
+        strlen(DllPath) + 1, NULL)) {
+        printf("[-] Error while writing dll path : %lu\n", GetLastError());
+        VirtualFreeEx(processInfo.hProcess, pDllPath, 0, MEM_RELEASE);
+        TerminateProcess(processInfo.hProcess, 1);
+        CloseHandle(processInfo.hThread);
+        CloseHandle(processInfo.hProcess);
+        return false;
+    }
+
+    return true;
 }
 
 int main() {
@@ -30,6 +67,6 @@ int main() {
         printf("[!] Please check the paths and try again.\n");
         return EXIT_FAILURE;
     }
-    
+
     return DllInjector(DllPath, ExecutablePath) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
