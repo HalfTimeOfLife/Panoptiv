9f4bbcd6fc61dbe765256ac078ceddf11b176e38
HalfTimeOfLife <ec.charbonnier@gmail.com>
2025-06-19 18:11:10 +0200

    SERVICES doesn't work + MAC is bugged

--- PanoptivDLL/HookedTypes.h
+++ PanoptivDLL/HookedTypes.h
@@ -7,6 +7,8 @@
 #include <powrprof.h>
 #include <psapi.h>
 
+#define NUMBER_OF_SERVICES 50
+
 typedef int (WINAPI* MessageBoxA_s)(HWND, LPCSTR, LPCSTR, UINT);
 MessageBoxA_s OriginalMessageBoxA = NULL;
 BYTE originalBytes_MessageBoxA[5];
@@ -17,21 +19,27 @@ BYTE originalBytes_GetForegroundWindow[5];
 
 typedef BOOLEAN(WINAPI* GetPwrCapabilities_s)(PSYSTEM_POWER_CAPABILITIES);
 GetPwrCapabilities_s OriginalGetPwrCapabilities = NULL;
+BYTE originalBytes_GetPwrCapabilities[5];
 
 typedef BOOL(WINAPI* GetDiskFreeSpaceExA_s)(LPCSTR, PULARGE_INTEGER, PULARGE_INTEGER, PULARGE_INTEGER);
 GetDiskFreeSpaceExA_s OriginalGetDiskFreeSpaceExA = NULL;
+BYTE originalBytes_GetDiskFreeSpaceExA[5];
 
 typedef DWORD(WINAPI* GetAdaptersInfo_s)(PIP_ADAPTER_INFO, PULONG);
 GetAdaptersInfo_s OriginalGetAdaptersInfo = NULL;
+BYTE originalBytes_GetAdaptersInfo[5];
 
 typedef BOOL(WINAPI* GlobalMemoryStatusEx_s)(LPMEMORYSTATUSEX);
 GlobalMemoryStatusEx_s OriginalGlobalMemoryStatusEx = NULL;
 
 typedef BOOL(WINAPI* GetCursorPos_s)(LPPOINT);
 GetCursorPos_s OriginalGetCursorPos = NULL;
+POINT lastFakePoint = { 100, 100 };
+BYTE originalBytes_GetCursorPos[5];
 
 typedef void (WINAPI* GetSystemInfo_s)(LPSYSTEM_INFO);
 GetSystemInfo_s OriginalGetSystemInfo = NULL;
+BYTE originalBytes_GetSystemInfo[5];
 
 typedef LSTATUS(WINAPI* RegQueryValueExA_s)(HKEY, LPCSTR, LPDWORD, LPDWORD, LPBYTE, LPDWORD);
 RegQueryValueExA_s OriginalRegQueryValueExA = NULL;
@@ -50,5 +58,6 @@ typedef BOOL(WINAPI* EnumServicesStatus_s)(
     LPDWORD lpResumeHandle
     );
 EnumServicesStatus_s OriginalEnumServicesStatus = NULL;
+BYTE originalBytes_EnumServicesStatus[5];
 
 #endif /* HOOKS_H */
--- PanoptivDLL/dllmain.c
+++ PanoptivDLL/dllmain.c
@@ -13,7 +13,6 @@
 
 #include "pch.h"
 #include "HookedTypes.h"
-
 #include "Helper.h"
 
 /**
@@ -65,23 +64,153 @@ HWND WINAPI HookedGetForegroundWindow(void)
 
 /*
 * @brief Function used for hooking the function GetCursorPos.
+* 
+* This function modifies the cursor position by adding a small random offset to the last known position (real or fake).
+* 
+* @param lpPoint Pointer to a POINT structure that receives the screen coordinates of the cursor.
+* 
+* @return TRUE if the function succeeds, FALSE otherwise.
 */
-BOOL WINAPI HookedGetCursorPos(POINT point)
+BOOL WINAPI HookedGetCursorPos(LPPOINT lpPoint)
 {
-    DWORD oldProtect;
-    void* pFunction = (void*)OriginalMessageBoxA;
+    if (!lpPoint) return FALSE;
 
-    VirtualProtect(pFunction, 5, PAGE_EXECUTE_READWRITE, &oldProtect);
-    memcpy(pFunction, originalBytes_MessageBoxA, 5);
-    VirtualProtect(pFunction, 5, oldProtect, &oldProtect);
+    lastFakePoint.x += (generate_random_number() % 3) - 1;
+    lastFakePoint.y += (generate_random_number() % 3) + 1;
 
-    static int toggle = 0;
-	POINT fakePoint1 = { 100, 100 };
-	POINT fakePoint2 = { 200, 200 };
+    lpPoint->x = lastFakePoint.x;
+    lpPoint->y = lastFakePoint.y;
 
-    toggle = !toggle;
+    return TRUE;
+}
 
-    return toggle ? ((GetCursorPos_s)pFunction)(&fakePoint1) : ((GetCursorPos_s)pFunction)(&fakePoint2);
+/*
+* @brief Function used for hooking the function GetPwrCapabilities.
+* 
+* This function modifies the system power capabilities to always indicate that all power states are available 
+* and that the thermal control capacity is enable.
+* 
+* @param lpspc Pointer to a SYSTEM_POWER_CAPABILITIES structure that receives the power capabilities.
+* 
+* @return TRUE if the function succeeds, FALSE otherwise.
+*/
+BOOL WINAPI HookedGetPwrCapabilities(PSYSTEM_POWER_CAPABILITIES lpspc) {
+	if (!lpspc) return FALSE;
+
+	lpspc->SystemS1 = TRUE; 
+    lpspc->SystemS2 = TRUE;
+    lpspc->SystemS3 = TRUE;
+    lpspc->SystemS4 = TRUE;
+    lpspc->SystemS5 = TRUE;
+	lpspc->ThermalControl = TRUE;
+
+	return TRUE;
+}
+
+
+/*
+* @brief Function used for hooking the function GetDiskFreeSpaceExA.
+* 
+* This modifies the disk space information to always return a fixed amount of free and total bytes,
+* 
+* @param lpDirectoryName                Pointer to a string that specifies the directory on the disk.
+* @param lpFreeBytesAvailableToCaller   Pointer to a variable that receives the number of free bytes available to the caller.
+* @param lpTotalNumberOfBytes           Pointer to a variable that receives the total number of bytes on the disk.
+* @param lpTotalNumberOfFreeBytes       Pointer to a variable that receives the total number of free bytes on the disk.
+* 
+* @return TRUE if the function succeeds, FALSE otherwise.
+*/
+BOOL WINAPI HookedGetDiskFreeSpaceExA(LPCSTR lpDirectoryName, PULARGE_INTEGER lpFreeBytesAvailableToCaller, PULARGE_INTEGER lpTotalNumberOfBytes, PULARGE_INTEGER lpTotalNumberOfFreeBytes) {
+
+	if (!lpDirectoryName || !lpFreeBytesAvailableToCaller || !lpTotalNumberOfBytes || !lpTotalNumberOfFreeBytes) return FALSE;
+
+    lpTotalNumberOfBytes->QuadPart = 1000000000000;
+    lpTotalNumberOfFreeBytes->QuadPart = 500000000000;
+
+    return TRUE;
+}
+
+/*
+* @brief Function used for hooking the function GetSystemInfo.
+* 
+* This function modifies the system information to always report a fixed number of processors equal to 16.
+*
+* @param lpSystemInfo Pointer to a SYSTEM_INFO structure that receives the system information.
+* 
+* @return TRUE if the function succeeds, FALSE otherwise.
+*/
+BOOL WINAPI HookedGetSystemInfo(LPSYSTEM_INFO lpSystemInfo) {
+	if (!lpSystemInfo) return FALSE;
+
+    lpSystemInfo->dwNumberOfProcessors = 16;
+
+    return TRUE;
+}
+
+/*
+* @brief Function used for hooking the function EnumServicesStatus.
+* 
+* This function modifies the service names returned by EnumServicesStatus to always return "bidule" for each service.
+* 
+* @param hSCManager         Handle to the service control manager database.
+* @param dwServiceType      Type of services to be enumerated. This can be a combination of SERVICE_* flags.
+* @param dwServiceState     State of services to be enumerated. This can be a combination of SERVICE_STATE_* flags.
+* @param lpServices         Pointer to an array of ENUM_SERVICE_STATUS structures that receives the status information for the services.
+* @param cbBufSize          Size of the buffer pointed to by lpServices, in bytes.
+* @param pcbBytesNeeded     Pointer to a variable that receives the number of bytes needed to store all service status information.
+* @param lpServicesReturned Pointer to a variable that receives the number of services returned in the lpServices buffer.
+* @param lpResumeHandle     Pointer to a variable that receives a resume handle for the next call to EnumServicesStatus.
+* 
+* @return TRUE if the function succeeds, FALSE otherwise.
+*/
+
+BOOL WINAPI HookedEnumServicesStatus(SC_HANDLE hSCManager, DWORD dwServiceType, DWORD dwServiceState, LPENUM_SERVICE_STATUS lpServices, DWORD cbBufSize, LPDWORD pcbBytesNeeded, LPDWORD lpServicesReturned, LPDWORD lpResumeHandle) {
+	if (!hSCManager || !lpServices || !pcbBytesNeeded || !lpServicesReturned || !lpResumeHandle) return FALSE;
+
+	if (!OriginalEnumServicesStatus) return FALSE;
+
+    ENUM_SERVICE_STATUSA fakeServiceName[NUMBER_OF_SERVICES];
+
+	SERVICE_STATUS customServiceStatus = { SERVICE_WIN32_OWN_PROCESS, SERVICE_RUNNING, SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN, NO_ERROR, 0, 0, 0};
+
+    int i;
+    for(i = 0; i < NUMBER_OF_SERVICES; i++) {
+        fakeServiceName[i].lpServiceName = "bidule";
+        fakeServiceName[i].lpDisplayName = "bidule";
+		fakeServiceName[i].ServiceStatus = customServiceStatus;
+	}
+
+    lpServices = fakeServiceName;
+
+    return TRUE;
+}
+
+
+/*
+* @brief Function used for hooking the function GetAdaptersInfo.
+* 
+* This function modifies the MAC addresses of network adapters to a fixed value.
+* 
+* @param pAdapterInfo Pointer to a buffer that receives the adapter information.
+* @param pOutBufLen   Pointer to a variable that specifies the size of the buffer pointed to by pAdapterInfo, and receives the size of the data copied to the buffer.
+* 
+* @return TRUE if the function succeeds, FALSE otherwise.
+*/
+DWORD WINAPI HookedGetAdaptersInfo(PIP_ADAPTER_INFO pAdapterInfo, PULONG pOutBufLen) {
+    if (!pAdapterInfo || !pOutBufLen) return ERROR_NOT_SUPPORTED;
+    if (!OriginalGetAdaptersInfo) return ERROR_NOT_SUPPORTED;
+
+    while (pAdapterInfo) {
+        pAdapterInfo->Address[0] = 0x12;
+        pAdapterInfo->Address[1] = 0x34;
+        pAdapterInfo->Address[2] = 0x56;
+        pAdapterInfo->Address[3] = 0x78;
+        pAdapterInfo->Address[4] = 0x91;
+        pAdapterInfo->Address[5] = 0x00;
+        pAdapterInfo = pAdapterInfo->Next;
+    }
+
+    return ERROR_SUCCESS;
 }
 
 /**
@@ -137,8 +266,14 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
         set_console_color(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
         printf("[+] DLL injected !\n");
         setvbuf(stdout, NULL, _IONBF, 0);
-        //InstallHook("user32.dll", "MessageBoxA", HookedMessageBoxA, originalBytes_MessageBoxA, (void**)&OriginalMessageBoxA);
+        /* InstallHook("user32.dll", "MessageBoxA", HookedMessageBoxA, originalBytes_MessageBoxA, (void**)&OriginalMessageBoxA); */
         InstallHook("user32.dll", "GetForegroundWindow", HookedGetForegroundWindow, originalBytes_GetForegroundWindow, (void**)&OriginalGetForegroundWindow);
+		InstallHook("user32.dll", "GetCursorPos", HookedGetCursorPos, originalBytes_GetCursorPos, (void**)&OriginalGetCursorPos);
+		InstallHook("powrprof.dll", "GetPwrCapabilities", HookedGetPwrCapabilities, originalBytes_GetPwrCapabilities, (void**)&OriginalGetPwrCapabilities);
+		InstallHook("kernel32.dll", "GetDiskFreeSpaceExA", HookedGetDiskFreeSpaceExA, originalBytes_GetDiskFreeSpaceExA, (void**)&OriginalGetDiskFreeSpaceExA);
+        InstallHook("kernel32.dll", "GetSystemInfo", HookedGetSystemInfo, originalBytes_GetSystemInfo, (void**)&OriginalGetSystemInfo);
+		InstallHook("advapi32.dll", "EnumServicesStatus", HookedEnumServicesStatus, originalBytes_EnumServicesStatus, (void**)&OriginalEnumServicesStatus);
+		InstallHook("iphlpapi.dll", "GetAdaptersInfo", HookedGetAdaptersInfo, originalBytes_GetAdaptersInfo, (void**)&OriginalGetAdaptersInfo);
         break;
 
     case DLL_THREAD_ATTACH:
